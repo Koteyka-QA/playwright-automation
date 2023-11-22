be8d03f8e8e048f355e797544f288976220fa57a
Nataliia Iskanderova <natali.iskanderova@gmail.com>
2023-11-22 12:24:29 +0200

    init playwright

---
+++ another/asserts_demo.py
@@ -0,0 +1,11 @@
+def test_asserts():
+    print('before' * 5)
+    result = False
+    print('after' * 5)
+    assert result, 'the tests is failed'
+
+def test_new():
+    x = 5
+    assert 1 == 1
+def test2():
+    assert 2 == 2
